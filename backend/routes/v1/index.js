048dc36f67b27b11236619d68252506b3630449b
wilsterdc <delacruz.wilster15@gmail.com>
2025-12-12 22:51:29 +0800

    fixing oauth session handling

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -8,21 +8,15 @@ const v1 = new Router();
 
 v1.use('/account', accountRouter);
 
-v1.use(passport.initialize())
-v1.use(passport.session())
 v1.get('/login/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
-v1.get('/login/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
+v1.get('/login/google/redirect', passport.authenticate('google'), (req, res) => {
     res.send('Redirected.')
-    console.log(req.user)
+    console.log(req.cookies)
+    console.log(req.user.id)
 });
-
-v1.get('/read-cookie', (req, res) => {
-    console.log(req);
-    res.json({cookie: req.cookies})
-})
 
 
 export default v1;
