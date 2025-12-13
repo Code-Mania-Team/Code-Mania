31b675a20022c9d120d7e4c198251ae29a6b2bb0
Johnpaulbodino <johnpaulbodino49@gmail.com>
2025-12-13 16:36:41 +0800

    backend

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -1,10 +1,30 @@
 import { Router } from 'express';
 import accountRouter from './accountRoutes.js';
+import passport from 'passport';
+import refreshRouter from './refreshRoute.js';
 
 //import homeRouter from './homeRoutes.js';
 
 const v1 = new Router();
 
 v1.use('/account', accountRouter);
+v1.use('/token', refreshRouter);
+
+v1.use(passport.initialize())
+v1.use(passport.session())
+v1.get('/login/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+v1.get('/login/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
+    res.send('Redirected.')
+    console.log(req.user)
+});
+
+v1.get('/read-cookie', (req, res) => {
+    console.log(req);
+    res.json({cookie: req.cookies})
+})
+
 
 export default v1;
