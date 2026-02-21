11bf414923defec2f0f8de857e06211e633a7de3
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-02-22 01:45:36 +0800

    labas mo ang merge

--- backend/models/gameData.js
+++ backend/models/gameData.js
@@ -1,8 +1,9 @@
-import { supabase } from "../core/supabaseClient.js";   
-
-
+import { supabase } from "../core/supabaseClient.js";
 
 class GameData {
+  constructor() {
+    this.db = supabase;
+  }
 
   async getUserGameData(user_id, programming_language_id) {
     const { data, error } = await this.db
@@ -16,103 +17,12 @@ class GameData {
         )
         `)
         .eq("user_id", user_id)
-        .eq("status", "completed")
         .eq("quests.programming_language_id", programming_language_id);
 
-        this.db = supabase;
+    if (error) throw error;
 
+    return data || [];
     }
-
-
-
-    async getUserGameData(user_id, programming_language) {
-
-        const { data, error } = await this.db
-
-            .from("users_game_data")
-
-            .select("exercise_id, xp_earned, programming_language")
-
-            .eq("user_id", user_id)
-
-            .eq("programming_language", programming_language);
-
-
-
-        if (error) throw error;
-
-        return data;
-
-        }
-
-
-
-
-
-    async createUserGameData({ user_id, xp_earned, exercise_id , programming_language}) {
-
-        const { data, error } = await this.db
-
-            .from("users_game_data")
-
-            .insert({
-
-                user_id,
-
-                xp_earned,
-
-                exercise_id,
-
-                programming_language: programming_language,
-
-                created_at: new Date().toISOString()
-
-            })
-
-            .select("*")
-
-            .maybeSingle();
-
-
-
-        if (error) throw error;
-
-        return data;
-
-    }
-
-
-
-    async updateUserGameData({ user_id, game_id, xp_earned, exercise_id }) {
-
-        const { data, error } = await this.db
-
-            .from("users_game_data")
-
-            .update({
-
-                xp_earned,
-
-                exercise_id,
-
-            })
-
-            .eq("user_id", user_id)
-
-            .eq("game_id", game_id)
-
-            .select("*")
-
-            .maybeSingle();
-
-
-
-        if (error) throw error;
-
-        return data;
-
-    }
-
 
 }
 
--- frontend/src/utilities/engine/scenes/gameScene.js
+++ frontend/src/utilities/engine/scenes/gameScene.js
@@ -43,7 +43,7 @@ export default class GameScene extends Phaser.Scene {
 
     if (!this.quest || !this.quest.map_id) {
       console.error("❌ Quest missing or invalid. Using fallback map1.");
-      this.currentMapId = "map7";
+      this.currentMapId = "map9";
     } else {
       this.currentMapId = this.quest.map_id; // ✅ CORRECT FIELD
     }
