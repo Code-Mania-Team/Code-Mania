d69cf24b1bb9edcc4854be320e0f333a5fb6affe
wilsterdc <delacruz.wilster15@gmail.com>
2026-02-24 18:27:36 +0800

    Added rate limiter (backend)

---
+++ backend/middlewares/rateLimiter.js
@@ -0,0 +1,29 @@
+import rateLimit from "express-rate-limit";
+
+export function globalLimiter() {
+  return rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 50,
+    standardHeaders: true,
+    // legeacyHeaders: false,
+    handler: (req, res) => {
+      res.status(429).json({
+        status: "error",
+        message: "Rate limit exceeded",
+      });
+    },
+  });
+}
+
+export function authLimiter() {
+  return rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10,
+    handler: (req, res) => {
+      res.status(429).json({
+        status: "error",
+        message: "Too many login attemps.",
+      });
+    },
+  });
+}
--- backend/routes/v1/accountRoutes.js
+++ backend/routes/v1/accountRoutes.js
@@ -1,9 +1,10 @@
 // routes/v1/accountRoutes.js
-import express from 'express';
-import AccountController from '../../controllers/v1/accountController.js';
-import { authentication } from '../../middlewares/authentication.js';
-import { authorization } from '../../middlewares/authorization.js';
-import passport from 'passport';
+import express from "express";
+import AccountController from "../../controllers/v1/accountController.js";
+import { authentication } from "../../middlewares/authentication.js";
+import { authorization } from "../../middlewares/authorization.js";
+import passport from "passport";
+import { authLimiter } from "../../middlewares/rateLimiter.js";
 
 const accountRouter = express.Router();
 const account = new AccountController();
@@ -12,35 +13,55 @@ const account = new AccountController();
 // accountRouter.use(authorization);
 
 // Request OTP (signup or login) – single endpoint
-accountRouter.post('/signup/request-otp', account.requestOtp.bind(account));
+accountRouter.post("/signup/request-otp", account.requestOtp.bind(account));
 
 // Verify OTP after user clicks or enters it
-accountRouter.post('/signup/verify-otp', account.verifyOtp.bind(account));
+accountRouter.post("/signup/verify-otp", account.verifyOtp.bind(account));
 
 // Set username (requires authentication)
-accountRouter.post('/setOnboarding', authentication, account.setUsernameAndCharacter.bind(account));
+accountRouter.post(
+  "/setOnboarding",
+  authentication,
+  account.setUsernameAndCharacter.bind(account),
+);
 
-accountRouter.post('/login', account.login.bind(account));
+accountRouter.post("/login", authLimiter(), account.login.bind(account));
 
-accountRouter.get('/login/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
+accountRouter.get(
+  "/login/google",
+  authLimiter(),
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+);
 
-accountRouter.get('/login/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
+accountRouter.get(
+  "/login/google/redirect",
+  passport.authenticate("google", { session: false }),
+  (req, res) => {
     account.googleLogin(req, res);
-});
+  },
+);
 
-accountRouter.post('/logout', account.logout.bind(account));
+accountRouter.post("/logout", account.logout.bind(account));
 
 // Get current user's profile
-accountRouter.get('/', authentication, account.profile.bind(account));
+accountRouter.get("/", authentication, account.profile.bind(account));
 
 // Update profile (username/full_name)
-accountRouter.patch('/', authentication, account.updateProfile.bind(account));
+accountRouter.patch("/", authentication, account.updateProfile.bind(account));
 
 // Delete account
-accountRouter.delete('/', authentication, account.deleteUser.bind(account));
-accountRouter.get('/summary', authentication, account.getProfileSummary.bind(account));
-accountRouter.get('/learning-progress', authentication, account.getLearningProgress.bind(account));
+accountRouter.delete("/", authentication, account.deleteUser.bind(account));
+accountRouter.get(
+  "/summary",
+  authentication,
+  account.getProfileSummary.bind(account),
+);
+accountRouter.get(
+  "/learning-progress",
+  authentication,
+  account.getLearningProgress.bind(account),
+);
 
 export default accountRouter;
