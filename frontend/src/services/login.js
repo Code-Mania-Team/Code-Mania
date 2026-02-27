b656e378dbe17cbb29553ef0f6fb77f8d378794b
wilsterdc <delacruz.wilster15@gmail.com>
2026-02-27 20:35:14 +0800

    Google Oauth Implemented

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -1,14 +1,13 @@
 import { axiosPublic } from "../api/axios";
 
 const login = async (email, password) => {
-
   try {
     const response = await axiosPublic.post(
       "/v1/account/login",
       { email, password },
       {
         headers: {},
-      }
+      },
     );
 
     console.log("Login response:", response.data);
@@ -21,19 +20,18 @@ const login = async (email, password) => {
     return response.data;
   } catch (error) {
     const backendMessage =
-      error?.response?.data?.message ||
-      error?.response?.data?.error;
+      error?.response?.data?.message || error?.response?.data?.error;
     if (backendMessage) {
       throw new Error(backendMessage);
     }
     console.error("Login error:", error.message);
     throw error;
   }
-
 };
 
 const loginWithGoogle = async () => {
-  window.location.href = "http://localhost:3000/v1/account/login/google"
-}
+  window.location.href =
+    "https://code-mania-production.up.railway.app/v1/account/login/google";
+};
 
 export { login, loginWithGoogle };
