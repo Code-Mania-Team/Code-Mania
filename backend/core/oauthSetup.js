1bfa69b8f29370284f4d72ed3f9ae0870f1f2754
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-03-10 18:21:26 +0800

    push

--- backend/core/oauthSetup.js
+++ backend/core/oauthSetup.js
@@ -6,7 +6,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/v1/account/login/google/redirect',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
