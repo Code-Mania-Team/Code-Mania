f6e998148a5f2b2ffe9d4f17a05661ac51314401
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-03-19 03:55:48 +0800

    awit

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -1,17 +1,34 @@
 import rateLimit from "express-rate-limit";
 
-export function authLimiter() {
+export function globalLimiter() {
   return rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 8, // 8 requests per minute
-
+    windowMs: 1 * 60 * 1000,
+    max: 100, // more realistic global limit
     standardHeaders: true,
     legacyHeaders: false,
 
     handler: (req, res) => {
       res.status(429).json({
         success: false,
-        message: "Too many requests. Please try again in 1 minute.",
+        message: "Too many requests. Please try again later.",
+      });
+    },
+  });
+}
+
+export function authLimiter() {
+  return rateLimit({
+    windowMs: 60 * 1000,
+    max: 8,
+
+    standardHeaders: true,
+    legacyHeaders: false,
+    skipSuccessfulRequests: true,
+
+    handler: (req, res) => {
+      res.status(429).json({
+        success: false,
+        message: "Too many login attempts. Try again in 1 minute.",
       });
     },
   });
